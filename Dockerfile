042b34c8e9dea2b71c7a9565224f51524407c619
Philip Jay <phil@jay.id.au>
2018-07-12 23:24:09 +1000

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,31 @@
+FROM alpine
+
+MAINTAINER Philip Jay <phil@jay.id.au>
+
+ENV TZ Australia/Melbourne
+
+# Remote server details
+ENV USERNAME=""
+ENV PASSWORD=""
+ENV HOST=""
+ENV PORT=""
+ENV TARGET=""
+
+# Space separated list of local directories
+ENV LOCATIONS=""
+
+# TCP port knock as required
+ENV KNOCK1=""
+ENV KNOCK2=""
+
+RUN apk update \
+ && apk upgrade \
+ && apk add \
+      bash \
+      rsync \
+ && rm -rf /var/cache/apk/*
+
+ADD backup.sh /usr/local/sbin/backup
+RUN chmod +x /usr/local/sbin/backup
+
+CMD ["/usr/local/sbin/backup"]
---
+++ backup.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -u
+
+RSYNC=/bin/rsync
+
+EXIT=0
+
+if [[ -n ${KNOCK1} ]] ; then
+    curl -s --connect-timeout 2 http://${TARGET_HOST}:${KNOCK1}
+fi
+if [[ -n ${KNOCK1} ]] ; then
+    curl -s --connect-timeout 2 http://${TARGET_HOST}:${KNOCK2}
+fi
+
+for DIR in ${LOCATIONS} ; do
+    echo -e "INFO: New backup starting `date`"
+    rsync -HPax --delete --stats \
+          -e "sshpass -p ${PASSWORD} ssh -p ${PORT} -l ${USERNAME}" \
+          ${DIR} \
+          ${HOST}::${TARGET}${DIR}
+
+    RSYNC_EXIT=${?}
+    if [[ ${RSYNC_EXIT} -ne 0 ]] ; then
+        echo "ERROR: rsync exited non-zero for ${DIR}"
+        EXIT=${RSYNC_EXIT}
+    fi
+
+    echo -e "\n"
+done
+
+exit ${EXIT}
